baea27a2dc6cb9f76592a6b4bb2d915623404c15
Jeff Pieta <jeff@aislabs.com>
2019-11-02 16:55:20 -0500

    Initial commit nms web

---
+++ html/graph.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * LibreNMS
+ *
+ *   This file is part of LibreNMS.
+ *
+ * @package    librenms
+ * @subpackage graphing
+ * @copyright  (C) 2006 - 2012 Adam Armstrong
+ */
+
+use LibreNMS\Authentication\LegacyAuth;
+
+$start = microtime(true);
+
+$init_modules = array('web', 'graphs', 'auth');
+require realpath(__DIR__ . '/..') . '/includes/init.php';
+
+$auth = LegacyAuth::check() || is_client_authorized($_SERVER['REMOTE_ADDR']);
+
+if (!$auth) {
+    die('Unauthorized');
+}
+
+set_debug(isset($_GET['debug']));
+
+rrdtool_initialize(false);
+
+require $config['install_dir'] . '/includes/html/graphs/graph.inc.php';
+
+rrdtool_close();
+
+if ($debug) {
+    echo '<br />';
+    printf("Runtime %.3fs", microtime(true) - $start);
+    echo '<br />';
+    printStats();
+}
---
+++ resources/views/menu/custom.blade.php
@@ -0,0 +1,19 @@
+
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-hover="dropdown" data-toggle="dropdown"><i class="fa fa-star fa-fw fa-lg fa-nav-icons hidden-md" aria-hidden="true"></i>
+        <span class="hidden-sm">AIS</span></a>
+    <ul class="dropdown-menu">
+	@admin
+<li>
+    <a href="http://10.181.160.100/freeradius/mng-main.php"><i class="fa fa-star fa-fw fa-lg fa-nav-icons hidden-md" aria-hidden="true"></i>RADIUS</a>
+</li>
+@endadmin
+<li>
+    <a href="https://unifi.aislabs.net:8443"><i class="fa fa-anchor fa-fw fa-lg" aria-hidden="true"></i>Unifi</a>
+</li>
+<li>
+    <a href="https://security.aislabs.com"><i class="fa fa-database fa-fw fa-lg" aria-hidden="true"></i>SIEM</a>
+</li>
+   </ul>
+</li>
+
